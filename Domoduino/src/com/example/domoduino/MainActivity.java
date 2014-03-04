88f56fc941492a3531eba02046cc89a83dec47ce
Ofelia <ofeliamoral92@gmail.com>
2014-03-04 16:24:36 +0100

    Conectar bluetooth completo

--- Domoduino/src/com/example/domoduino/MainActivity.java
+++ Domoduino/src/com/example/domoduino/MainActivity.java
@@ -1,58 +1,74 @@
 package com.example.domoduino;
 
+import java.util.ArrayList;
+import java.util.Set;
+
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
+	protected static final int REQUEST_ENABLE_BT = 0;
 	private Button btnBluetooth; 
 	private BluetoothAdapter bAdapter;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-				// Obtenemos el adaptador Bluetooth. Si es NULL, significara que el
-				// dispositivo no posee Bluetooth, por lo que deshabilitamos el boton
-				// encargado de activar/desactivar esta caracteristica.
+		//Get Bluettoth Adapter
+				bAdapter = BluetoothAdapter.getDefaultAdapter();
+				
 				btnBluetooth = (Button)findViewById(R.id.btnBluetooth);
 				btnBluetooth.setOnClickListener(new OnClickListener() 
 		        {
 
 					@Override
 					public void onClick(View v) {
-						// TODO Auto-generated method stub
-
-						bAdapter = BluetoothAdapter.getDefaultAdapter();
-						if(bAdapter.isEnabled())
-						{
-						    btnBluetooth.setText("desactivado");
-						    bAdapter.disable();
+						
+						//Notificación visual conectando...
+						Toast toast1 = Toast.makeText(getApplicationContext(),"Conectando...", Toast.LENGTH_SHORT);
+						
+						// Check smartphone support Bluetooth
+						if(bAdapter == null){
+							//Device does not support Bluetooth
+							Toast.makeText(getApplicationContext(), "Not support bluetooth", 5).show();
+							finish();
+						}
+						
+						// Check Bluetooth enabled
+						if(!bAdapter.isEnabled()){
+							Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+							startActivityForResult(enableBtIntent, 1);
+							toast1.show();
+							btnBluetooth.setText("Desconectar");
 						}
 						else
 						{
-						    btnBluetooth.setText("activado");
-							bAdapter.enable();
+							btnBluetooth.setText("Conectar");
+							bAdapter.disable();
 						}
+						
+						
 					}
 					
 		        });
-				
-				//Prueba botón pulsar
-				/*button1 = (Button) findViewById(R.id.button1);
-				button1.setOnClickListener(new OnClickListener() 
-		        {
-					
-		        });*/
 	}
+	
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
