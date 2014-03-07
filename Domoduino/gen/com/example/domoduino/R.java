2019394477cc2d8e93cd2a3047ccbef1787f14b5
Angela <angelasojuela@gmail.com>
2014-03-07 13:08:08 +0100

    nueva pantalla

--- Domoduino/gen/com/example/domoduino/R.java
+++ Domoduino/gen/com/example/domoduino/R.java
@@ -38,6 +38,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int pantallaprincipal=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -47,6 +48,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int titulo=0x7f050004;
     }
     public static final class style {
         /** 
