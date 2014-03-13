09b63f303d696fb7874fff0bd6be1d2bbdf468fe
Angela <angelasojuela@gmail.com>
2014-03-13 17:33:58 +0100

    icono tamaño

--- Domoduino/gen/com/example/domoduino/R.java
+++ Domoduino/gen/com/example/domoduino/R.java
@@ -21,23 +21,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int a=0x7f020000;
-        public static final int b=0x7f020001;
-        public static final int f=0x7f020002;
-        public static final int fondo=0x7f020003;
-        public static final int fondoo=0x7f020004;
-        public static final int fondop=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int jj=0x7f020007;
-        public static final int logo=0x7f020008;
-        public static final int mueco=0x7f020009;
-        public static final int muneco1=0x7f02000a;
-        public static final int ruedabombillaapagada=0x7f02000b;
-        public static final int ruedasacelerometro=0x7f02000c;
-        public static final int ruedasbombillaencendida=0x7f02000d;
-        public static final int ruedasreloj=0x7f02000e;
-        public static final int ruedasstart=0x7f02000f;
-        public static final int ruedasstop=0x7f020010;
+        public static final int f=0x7f020000;
+        public static final int fondop=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icono=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int ruedabombillaapagada=0x7f020005;
+        public static final int ruedasacelerometro=0x7f020006;
+        public static final int ruedasbombillaencendida=0x7f020007;
+        public static final int ruedasreloj=0x7f020008;
+        public static final int ruedasstart=0x7f020009;
+        public static final int ruedasstop=0x7f02000a;
     }
     public static final class id {
         public static final int action_settings=0x7f080007;
