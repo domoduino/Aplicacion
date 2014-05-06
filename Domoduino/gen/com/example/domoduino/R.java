9537466edba91d61cf17798558344ddf8f0901b6
Angela <angelasojuela@gmail.com>
2014-05-06 16:58:31 +0200

    ultimaaa

--- Domoduino/gen/com/example/domoduino/R.java
+++ Domoduino/gen/com/example/domoduino/R.java
@@ -48,30 +48,31 @@ public final class R {
         public static final int stop=0x7f020018;
     }
     public static final class id {
-        public static final int Ajustes=0x7f080016;
-        public static final int Ajustes1=0x7f080017;
-        public static final int Ajustes2=0x7f080018;
-        public static final int Ayuda=0x7f080019;
-        public static final int ListView_listado=0x7f080014;
+        public static final int Ajustes=0x7f080017;
+        public static final int Ajustes1=0x7f080018;
+        public static final int Ajustes2=0x7f080019;
+        public static final int Ayuda=0x7f08001a;
+        public static final int ListView_listado=0x7f080015;
         public static final int RelativeLayout1=0x7f080000;
         public static final int btnConectar=0x7f08000c;
         public static final int devicesfound=0x7f08000b;
-        public static final int imageButton1=0x7f08000d;
-        public static final int imageButton2=0x7f08000e;
-        public static final int imageButton3=0x7f08000f;
-        public static final int imageButton4=0x7f080010;
-        public static final int imageButton5=0x7f080011;
+        public static final int imageButton1=0x7f08000e;
+        public static final int imageButton2=0x7f08000f;
+        public static final int imageButton3=0x7f080010;
+        public static final int imageButton4=0x7f080011;
+        public static final int imageButton5=0x7f080012;
         public static final int imageView1=0x7f080001;
-        public static final int imageView_imagen=0x7f080013;
+        public static final int imageView_imagen=0x7f080014;
         public static final int imagen1=0x7f080004;
         public static final int imagen2=0x7f080005;
         public static final int imagen21=0x7f080008;
         public static final int imagen22=0x7f080009;
         public static final int imagen3=0x7f080006;
         public static final int imagen4=0x7f080007;
+        public static final int textAlarma=0x7f08000d;
         public static final int textAyuda=0x7f08000a;
-        public static final int textView_inferior=0x7f080015;
-        public static final int textView_superior=0x7f080012;
+        public static final int textView_inferior=0x7f080016;
+        public static final int textView_superior=0x7f080013;
         public static final int textview1=0x7f080002;
         public static final int timePicker1=0x7f080003;
     }
